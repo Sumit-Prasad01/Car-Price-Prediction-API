032c2c2b4bd6498b1e2b1da587164b33ed38a3be
Sumit prasad <sumitprasadd.wb24@gmail.com>
2026-04-03 16:41:10 +0530

    add ml integration

---
+++ app/services/model_service.py
@@ -0,0 +1,26 @@
+import joblib
+import pandas as pd
+
+from app.core.config import settings
+from app.core.paths import MODEL_PATH
+from app.cache.redis_cache import set_cached_prediction, get_cached_prediction
+
+
+model = joblib.load(MODEL_PATH)
+
+
+def predict_car_price(data : dict):
+    
+    cache_key = " ".join([str(val) for val in  data.values()])
+    cached = get_cached_prediction(cache_key)
+
+    if cached:
+        return cached
+    
+    input_data = pd.DataFrame([data])
+
+    prediction = model.predict(input_data)[0]
+
+    set_cached_prediction(cache_key, prediction)
+
+    return prediction
